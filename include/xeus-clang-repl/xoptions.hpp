4fb3b8996b8daed55a8eabb693f9d10209b4662b
ioanaif <ioana.ifrim@gmail.com>
2022-10-11 17:39:58 +0300

    Initial commit for a clang-repl based xeus kernel.
    This patch was developed by Ioana Ifrim (@ioanaif) and
    Alexander Penev (@alexander-penev).

---
+++ include/xeus-clang-repl/xoptions.hpp
@@ -0,0 +1,29 @@
+//===-------------- xoptions.hpp - Option Parsing ---------------*- C++ -*-===//
+//
+// Licensed under the Apache License v2.0.
+// SPDX-License-Identifier: Apache-2.0
+//
+// The full license is in the file LICENSE, distributed with this software.
+//
+//===----------------------------------------------------------------------===//
+//
+// This file is does option parsing based on cxxopts.
+// FIXME: Replace with the llvm option parsing in CommandLine
+//
+//===----------------------------------------------------------------------===//
+
+#ifndef XCPP_OPTIONS_HPP
+#define XCPP_OPTIONS_HPP
+
+#include <string>
+
+#include "cxxopts.hpp"
+
+namespace xcpp {
+struct xoptions : public cxxopts::Options {
+  using parent = cxxopts::Options;
+  using parent::Options;
+  cxxopts::ParseResult parse(const std::string &line);
+};
+} // namespace xcpp
+#endif
